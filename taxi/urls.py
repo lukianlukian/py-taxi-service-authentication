b118b56d52a7be2763bb5e085b224037ac5ceaea
Lukian <lukian55d@gmail.com>
2026-02-28 00:22:13 +0000

    bad lukian stupd

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
 
 from .views import (
     index,
@@ -22,6 +23,10 @@ urlpatterns = [
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
+    path("accounts/login/", LoginView.as_view(
+        template_name="registration/login.html"
+    ), name="login"),
+    path("accounts/logout/", LogoutView.as_view(), name="logout"),
 ]
 
 app_name = "taxi"
